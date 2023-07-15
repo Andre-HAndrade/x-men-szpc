186ec399c4ca38ee27aca1b63f9ab7abf6537068
Andre-HAndrade <138685581+Andre-HAndrade@users.noreply.github.com>
2023-07-15 12:47:46 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -46,24 +46,23 @@ personagens.forEach((personagem) => {
   });
 });
 function alterarDescricaoPersonagem(personagem) {
-    const descricaoPersonagem = document.getElementById("descricao-personagem");
-    descricaoPersonagem.innerText = personagem.getAttribute("data-description");
+  const descricaoPersonagem = document.getElementById("descricao-personagem");
+  descricaoPersonagem.innerText = personagem.getAttribute("data-description");
 }
 
 function alterarNomePersonagemSelecionado(personagem) {
-    const nomePersonagem = document.getElementById("nome-personagem");
-    nomePersonagem.innerText = personagem.getAttribute("data-name");
+  const nomePersonagem = document.getElementById("nome-personagem");
+  nomePersonagem.innerText = personagem.getAttribute("data-name");
 }
 
 function alterarImagemPersonagemSelecionado(personagem) {
-    const imagemPersonagemGrande = document.querySelector(".personagem-grande");
-    // passo 2 - alterar a imagem do personagem grande
-    const idPersonagem = personagem.attributes.id.value;
-    imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
+  const imagemPersonagemGrande = document.querySelector(".personagem-grande");
+  // passo 2 - alterar a imagem do personagem grande
+  const idPersonagem = personagem.attributes.id.value;
+  imagemPersonagemGrande.src = `./src/imagens/card-${idPersonagem}.png`;
 }
 
 function removerSelecaoDoPersonagem() {
-    const personagemSelecionado = document.querySelector(".selecionado");
-    personagemSelecionado.classList.remove("selecionado");
+  const personagemSelecionado = document.querySelector(".selecionado");
+  personagemSelecionado.classList.remove("selecionado");
 }
-
